8f33f7fc81dad3632c03368c3f8e7f3d90d73baa
Mauricio Mutte <litiobr@gmail.com>
2019-11-09 17:53:19 -0300

    fix(product): fix width and add text ellipsis

--- packages/client/src/components/Product/style.js
+++ packages/client/src/components/Product/style.js
@@ -3,23 +3,39 @@ import styled from 'styled-components';
 export const Product = styled.div`
 	display: flex;
 	border: 1px solid #ccc;
-	max-width: 340px;
-	margin: 1em;
-	padding: 1em;
+	margin: 10px 0;
 	border-radius: 5px;
+	flex-basis: 100%;
 `;
 
 export const Info = styled.div`
-	margin-right: 5px;
+	padding: 15px;
+	width: calc(100% - 120px);
+
+	@media (min-width: 800px) {
+		padding: 1em;
+	}
 `;
 
 export const Title = styled.h3`
 	font-size: 16px;
+	overflow: hidden;
+	text-overflow: ellipsis;
+	display: -webkit-box;
+	-webkit-line-clamp: 1;
+	-webkit-box-orient: vertical;
 	margin-bottom: 6px;
 `;
 
 export const Description = styled.p`
 	font-size: 12px;
+	overflow: hidden;
+	text-overflow: ellipsis;
+	display: -webkit-box;
+	-webkit-line-clamp: 2;
+	-webkit-box-orient: vertical;
+
+	overflow: hidden;
 	color: #0008;
 `;
 
@@ -27,12 +43,18 @@ export const Price = styled.span`
 	font-size: 13px;
 	display: block;
 	padding-top: 10px;
+	margin-top: auto;
 `;
 
 export const Image = styled.div`
+	width: 120px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+
 	img {
 		border-radius: 3px;
-		height: 100%;
+		height: 75px;
 		width: 100px;
 	}
 `;
