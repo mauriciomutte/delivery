f3d38431fa1c69692b66fadb59f93306ac40050b
Mauricio Mutte <litiobr@gmail.com>
2019-11-13 22:20:42 -0300

    fix(header): decrease height

--- packages/client/src/components/Header/styles.js
+++ packages/client/src/components/Header/styles.js
@@ -1,19 +1,19 @@
 import styled from 'styled-components';
 
 export const Header = styled.header`
-  display: flex;
-  border-bottom: 1px solid #ccc;
-  height: 55px;
-  align-items: center;
-  padding: 0 5%;
-  justify-content: center;
+	align-items: center;
+	border-bottom: 1px solid #ccc;
+	display: flex;
+	height: 50px;
+	justify-content: center;
+	padding: 0 5%;
 `;
 
 export const Logo = styled.h1`
-  font-weight: bold;
-  font-size: 25px;
+	font-size: 25px;
+	font-weight: bold;
 `;
 
 export const Cart = styled.button`
-  margin-left: auto;
+	margin-left: auto;
 `;
