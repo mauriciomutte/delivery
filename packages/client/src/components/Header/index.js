c5e06ef1e6095a0b4377ed748950fbba404b7723
Mauricio Mutte <litiobr@gmail.com>
2019-11-14 23:12:17 -0300

    feaft(header): add Basket in Header

--- packages/client/src/components/Header/index.js
+++ packages/client/src/components/Header/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 
 import IconCart from '../IconCart';
+import Basket from '../Basket';
 
 import * as S from './styles';
 
 export default () => {
-  return (
-    <S.Header>
-      <S.Logo>Delivery</S.Logo>
-      <S.Cart>
-        <IconCart />
-      </S.Cart>
-    </S.Header>
-  );
+	return (
+		<S.Header>
+			<S.Logo>Delivery</S.Logo>
+			<S.Cart>
+				<IconCart />
+			</S.Cart>
+			<Basket />
+		</S.Header>
+	);
 };
