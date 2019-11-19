d75ca32973a33dc18eeb464f2fba1f1e0c5db443
Mauricio Mutte <litiobr@gmail.com>
2019-11-18 22:38:46 -0300

    feat(app): add app

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -4,12 +4,12 @@ import Header from './components/Header';
 import GlobalStyles from './styles/Global';
 
 function App() {
-  return (
-    <>
-      <GlobalStyles />
-      <Header></Header>
-    </>
-  );
+	return (
+		<>
+			<GlobalStyles />
+			<Header></Header>
+		</>
+	);
 }
 
 export default App;
